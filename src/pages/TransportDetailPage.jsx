75c1a5f7874f5e90ada7454b573d98e1f30b88c8
Nur Lailatul Hidayah <nurlailatulhidayah45@gmail.com>
2024-10-21 13:57:07 +0700

    detail fastboat booking

---
+++ src/pages/TransportDetailPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TransportDetailPage = () => {
+  return (
+    <div>
+      <section>
+        {/* <div>
+        <img src="/image/fastboat/starfish-fast-cruise.jpg" className="img-fluid p-2" alt="..." />
+        </div> */}
+        <div className="text-center mt-3 mb-3" >
+        <img src="/image/fastboat/starfish-fast-cruise.jpg" style={{width: '30rem'}} className="rounded"  alt="..." />
+      </div>
+
+      
+      </section>
+    </div>
+  );
+};
+
+export default TransportDetailPage;
